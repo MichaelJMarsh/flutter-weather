ea59d3ea253d898fde80541668a3fa5420509ca8
Michael Marsh <88854496+MichaelJMarsh@users.noreply.github.com>
2025-02-25 22:14:23 -0600

    Android build issue

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,9 +9,9 @@ plugins {
 }
 
 android {
-    namespace = "com.example.flutter_weather"
+    namespace = "com.flutterweather"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -24,7 +24,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.flutter_weather"
+        applicationId = "com.flutterweather"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
